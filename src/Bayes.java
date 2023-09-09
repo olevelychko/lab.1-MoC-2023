95e4e687e9f5b77fe963bd82119decb1a570bb2b
Illya <mazylia6@gmail.com>
2023-09-09 21:31:24 +0300

    Added new function calculationC

--- src/Bayes.java
+++ src/Bayes.java
@@ -1,25 +1,39 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 public class Bayes {
 
+    public static void calculationC(ArrayList C)
+    {
+        HashMap<Integer,String> probC = new HashMap<>();
+
+    }
+
     public static void main(String[] args) throws Exception {
-        ArrayList<Double> a = new ArrayList<>();
+        ArrayList<Integer> cipherText = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader("C:\\table_01.csv"));
         String line = null;
         Scanner scanner = null;
-        int r = 1;
         while ((line = reader.readLine()) != null) {
             scanner = new Scanner(line);
             scanner.useDelimiter(",");
             while (scanner.hasNext()) {
                 String num = scanner.next();
-                a.add(Double.parseDouble(num));
+                cipherText.add(Integer.parseInt(num));
             }
         }
         reader.close();
-        System.out.println(a);
+        //System.out.println(a);
+        for(int i = 0; i < cipherText.size(); i++)
+        {
+            System.out.print(cipherText.get(i) + " ");
+            if(i % 20 == 0 && i != 0)
+            {
+                System.out.println("   ");
+            }
+        }
     }
 
 }
